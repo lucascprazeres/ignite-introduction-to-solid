246d9aaa90fd8eb62e31ec9e7610dd17104fae5d
Lucas Prazeres <olucascostadp@gmail.com>
2021-03-17 19:07:51 -0300

    finish challenge

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const foundUserById = this.usersRepository.findById(user_id);
+
+    if (!foundUserById) {
+      throw new Error("User does not exist.");
+    }
+
+    return foundUserById;
   }
 }
 
